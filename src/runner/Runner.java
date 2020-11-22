63dcb415de22c9112f394ddd9f2e5c5c135ffe70
Camilo426 <juan.hurtado03@uptc.edu.co>
2020-11-22 14:52:30 -0500

    as

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -2,7 +2,7 @@ package runner;
 
 
 import com.formdev.flatlaf.intellijthemes.FlatGradiantoMidnightBlueIJTheme;
-import controller.Controller;
+
 
 import javax.swing.*;
 
@@ -11,7 +11,7 @@ public class Runner {
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel( new FlatGradiantoMidnightBlueIJTheme() );
-		new Controller();
+
 		} catch( Exception ex ) {
 			System.err.println( "Failed to initialize LaF" );
 		}
